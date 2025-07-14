f0266ffcae45c1721735adff7a530aa67c7a0a0a
GitHub Action <action@github.com>
2025-07-14 02:04:48 +0000

    Bump version from 0.0.25 to 0.0.26

--- Core.lua
+++ Core.lua
@@ -5,7 +5,7 @@ local addonName, HealIQ = ...
 
 -- Create the main addon object
 HealIQ = HealIQ or {}
-HealIQ.version = "0.0.25"
+HealIQ.version = "0.0.26"
 HealIQ.debug = true
 
 -- Default settings
